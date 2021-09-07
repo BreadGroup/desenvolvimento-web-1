26cf9b0712d0fa046f0c9e0234e903fc7416e457
Isaque <isaquefsnunes@gmail.com>
2021-09-07 18:57:02 +0000

    FIX: GitHub page do not load

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import { ThemeProvider } from "styled-components";
 
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <ThemeProvider theme={ currentTheme }>
-      <BrowserRouter>
+      <HashRouter>
         <GlobalStyle />
 
         <Header themeName={themeName} setThemeName={setThemeName} />
@@ -31,7 +31,7 @@ function App() {
 
         {/* <Footer /> */}
 
-      </ BrowserRouter>
+      </ HashRouter>
     </ThemeProvider>
   );
 }
