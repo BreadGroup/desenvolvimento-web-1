6044e2747b0182f4b9ab36334d1388964ba401d1
Isaque <isaquefsnunes@gmail.com>
2021-09-13 21:42:43 -0400

    Add material UI, finish contact and about us pages, fixed linters config, deleted develop container config. Change from yarn to NPM

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,52 +1,151 @@
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ConnectWithoutContactOutlinedIcon from '@mui/icons-material/ConnectWithoutContactOutlined';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Slide from '@mui/material/Slide';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { useTheme } from '@mui/material/styles';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
 
-import { AboutUsIcon, ContactIcon, Container, DarkModeIcon, HomeIcon } from "./styles";
+import {
+  AppBar,
+  Drawer,
+  DrawerHeader,
+  Search,
+  SearchIconWrapper,
+  StyledInputBase,
+} from './style';
 
-import { ThemeName } from '../../styles/themes';
-
-interface Props {
-  themeName: ThemeName;
-  setThemeName: (newName: ThemeName) => void;
+export interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
 }
 
-const Header: React.FC<Props> = ({ themeName, setThemeName }) => {
-  function toggleTheme() {
-    setThemeName(themeName === 'light' ? 'dark' : 'light');
-  }
+interface Props {
+  children: React.ReactElement;
+}
+
+function HideOnScroll({ children }: Props) {
+  const trigger = useScrollTrigger();
 
   return (
-    <Container>
-      <header>
-        <nav className="navbar">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                <HomeIcon className='menu-icon' />
-                <span className="link-text">Home</span>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">
-                <ContactIcon className='menu-icon' />
-                <span className="link-text">Contact</span>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/aboutus">
-                <AboutUsIcon className='menu-icon' />
-                <span className="link-text">About Us</span>
-              </Link>
-            </li>
-            <li onClick={toggleTheme} className="nav-item nav-link" id="dark-mode">
-              <DarkModeIcon className='menu-icon' />
-              <span className="link-text">Dark Mode</span>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </Container>
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
   );
 }
 
-export default Header;
+export default function Header() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const menuInfo = [
+    {
+      label: 'Home',
+      icon: <HomeOutlinedIcon />,
+      path: '/',
+    },
+    {
+      label: 'Contact us',
+      icon: <ConnectWithoutContactOutlinedIcon />,
+      path: '/contactus',
+    },
+    {
+      label: 'About us',
+      icon: <InfoOutlinedIcon />,
+      path: '/aboutus',
+    },
+  ];
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box component="header">
+      <CssBaseline />
+      <HideOnScroll>
+        <AppBar position="fixed" open={open}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              sx={{
+                marginRight: '36px',
+                ...(open && { display: 'none' }),
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            >
+              {document.title}
+            </Typography>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </Search>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader>
+          <HideOnScroll>
+            <Typography variant="h6">Agrotóxicos</Typography>
+          </HideOnScroll>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {menuInfo.map((menuItem) => {
+            return (
+              <ListItemButton
+                to={menuItem.path}
+                component={RouterLink}
+                key={menuItem.label}
+              >
+                <ListItemIcon>{menuItem.icon}</ListItemIcon>
+                <ListItemText primary={menuItem.label} />
+              </ListItemButton>
+            );
+          })}
+        </List>
+      </Drawer>
+    </Box>
+  );
+}
--- src/pages/AboutUs/index.tsx
+++ src/pages/AboutUs/index.tsx
@@ -1,13 +1,109 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 import React from 'react';
 
-const AboutUs: React.FC = () => {
+import Delmir from '../../assets/Delmir.jpeg';
+import Fabio from '../../assets/Fabio.png';
+import Isaque from '../../assets/Isaque.png';
+import Rafael from '../../assets/Rafael.png';
+
+export default function MediaCard() {
   return (
-    <main>
-      <h1>
-        About us
-      </h1>
-    </main>
+    <Stack
+      alignItems="stretch"
+      sx={{
+        m: 1,
+        p: 3,
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+      }}
+    >
+      <Card
+        sx={{
+          p: 3,
+          maxWidth: '48%',
+          marginTop: '64px',
+          width: '48%',
+        }}
+      >
+        <CardMedia component="img" height="300" image={Delmir} alt="Delmir" />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Delmir da Costa Felipe
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Possui graduação em Química Bacharelado pela Universidade Federal de
+            Mato Grosso do Sul (1997), licenciatura em Química pelo Programa de
+            Formação de Professores para o Magistério da UCDB(1998) e Mestrado
+            em Educação pela Universidade Federal de Mato Grosso do Sul (2004).
+            Foi Diretor Geral do Instituto Federal de Educação, Ciência e
+            Tecnologia - IFMS no campus de Aquidauana/MS de 15/10/2010 a
+            29/04/2016. Foi Pró-Reitor de Ensino do IFMS de 23/052016 a
+            11/12/2019.Tem experiência na área de Química, com ênfase em Ensino
+            de Química, atuando principalmente nos seguintes temas: ensino de
+            química, aprendizagem significativa, tecnologias educacionais,
+            educação a distância, educação profissional, ensino e aprendizagem.
+            Atualmente é professor do Ensino, Básico, Técnico e Tecnológico no
+            campus Campo Grande do IFMS. Doutorando em Ensino de Ciências no
+            INFI da UFMS - início 2020. Conselheiro Suplente do Conselho
+            Superior do IFMS (Biênio 2013-2014). Presidente da Comissão de Ética
+            do IFMS (Biênio 2013-2015). Conselheiro Titular do Conselho Superior
+            (2015 e Biênio 2016 e 2017). Conselheiro Titular do Conselho de
+            Ensino, Pesquisa e Extensão da UEMS (2017-2020). Presidente do
+            Conselho de Ensino, Pesquisa e Extensão do IFMS (2017-2019). Membro
+            do Colegiado do Programa de Pós-Graduação em Ensino de Ciências -
+            INFI/UFMS (2021-2023)
+          </Typography>
+        </CardContent>
+      </Card>
+      <Card sx={{ p: 3, maxWidth: '48%', marginTop: '64px', width: '48%' }}>
+        <CardMedia component="img" height="300" image={Fabio} alt="Delmir" />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Fábio Luiz Faria da Silva
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Possui graduação em Engenharia da Computação - Faculdades Associadas
+            de São Paulo (2006), especialização pela FACEL em Docência em
+            Ciência e Tecnologia da Informação (2018) e especialização pela FIAR
+            em Licenciatura Pedagógica em História. Atualmente é professor
+            efetivo do IFMS. Tem experiência na área de Ciência da Computação,
+            com ênfase em Análise e Desenvolvimento/Desenvolvimento Web.
+          </Typography>
+        </CardContent>
+      </Card>
+      <Card sx={{ p: 3, maxWidth: '48%', marginTop: '64px', width: '48%' }}>
+        <CardMedia component="img" height="300" image={Isaque} alt="Delmir" />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Isaque Ferreira Silva Nunes
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Estudante do IFMS. Possui conhecimento basico - intermediário nas
+            linguagens de programação Java, C, Python. Possui conhecimento
+            básico nas tecnologias Docker, Spring boot. Atualmente está
+            aprendendo javascript, React, MongoDB e Node
+          </Typography>
+        </CardContent>
+      </Card>
+      <Card sx={{ p: 3, maxWidth: '48%', marginTop: '64px', width: '48%' }}>
+        <CardMedia component="img" height="300" image={Rafael} alt="Delmir" />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Rafael Todescato Freire Veiga
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Estudante do IFMS. Possui conhecimento básico/ Intermediário nas
+            linguagens de programação: Java, Kotlin, Python. Também já utilizou
+            Spring boot
+          </Typography>
+        </CardContent>
+      </Card>
+    </Stack>
   );
 }
-
-export default AboutUs;
